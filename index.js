f65648b5702a344f04d6b0af197da7b575827f84
Aktan Zhakypbekov <aktanzhakypbekovbusiness@gmail.com>
2021-07-18 23:47:09 +0600

    Build the main logic. Use factories and modules instead of constructor/class oo

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-let Gameboard = (function () {
+let player1Play = document.querySelector("#player1ImgContainerDiv");
+let player2Play = document.querySelector("#player2ImgContainerDiv");
+let gameOverMessage = document.querySelector("#gameOverMessage");
+let gameOverMessageContent = document.querySelector("#gameOverMessageContent");
+let chartDiv = document.querySelector("#chart");
+
+let Game = (function () {
   let chart = ["", "", "", "", "", "", "", "", ""];
   let markCounter = [];
-  //o or x choice functionality
   let oxPlayer1Choice = "";
   let oxPlayer2Choice = "";
+
   let oChoiceButton = document.querySelector("#oChoice");
   oChoiceButton.addEventListener("click", () => {
     oxPlayer1Choice = "o";
     oxPlayer2Choice = "x";
-  });
-  oChoiceButton.addEventListener("mousedown", () => {
-    oChoiceButton.style.cssText =
-      "text-shadow: 0 0 5px green; box-shadow: 0 0 5px green";
-  });
-  oChoiceButton.addEventListener("mouseup", () => {
-    oChoiceButton.style.cssText = "";
   });
 
   let xChoiceButton = document.querySelector("#xChoice");
@@ -22,85 +21,81 @@ let Gameboard = (function () {
     oxPlayer1Choice = "x";
     oxPlayer2Choice = "o";
   });
-  xChoiceButton.addEventListener("mousedown", () => {
-    xChoiceButton.style.cssText =
-      "text-shadow: 0 0 5px green; box-shadow: 0 0 5px green";
-  });
-  xChoiceButton.addEventListener("mouseup", () => {
-    xChoiceButton.style.cssText = "";
-  });
 
-  let player1Play = document.querySelector("#player1ImgContainerDiv");
-  let player2Play = document.querySelector("#player2ImgContainerDiv");
-
-  let startButton = document.querySelector("#startButton");
-  startButton.addEventListener("click", displayMarks);
-  startButton.addEventListener("mousedown", () => {
-    startButton.style.cssText =
-      "text-shadow: 0 0 5px green; box-shadow: 0 0 5px green";
-  });
-  startButton.addEventListener("mouseup", () => {
-    startButton.style.cssText = "";
-  });
-
-  //display marks functionality
-  function displayMarks() {
+  let restartButton = document.querySelector("#restartButton");
+  restartButton.addEventListener("click", () => {
+    markCounter = [];
+    chart = ["", "", "", "", "", "", "", "", ""];
+    oxPlayer1Choice = "";
+    oxPlayer2Choice = "";
     let chartCellList = document.querySelectorAll(".chartCell");
     chartCellList.forEach((cell) => {
-      cell.addEventListener("click", (e) => {
-        if (markCounter.length == 0) {
-          markCounter.push(oxPlayer1Choice);
-          cell.textContent = oxPlayer1Choice;
-          player2Play.style.cssText = "color: red";
-          chart[parseInt(cell.id)] = oxPlayer1Choice;
-        } else if (markCounter[markCounter.length - 1] === "x") {
-          markCounter.push("o");
-          cell.textContent = "o";
-          player2Play.style.cssText = "";
-          player1Play.style.cssText = "color: red";
-          chart[parseInt(cell.id)] = "o";
-        } else if (markCounter[markCounter.length - 1] === "o") {
-          markCounter.push("x");
-          cell.textContent = "x";
-          player1Play.style.cssText = "";
-          player2Play.style.cssText = "color: red";
-          chart[parseInt(cell.id)] = "x";
-        }
-        if (
-          (chart[0] == "x" && chart[1] == "x" && chart[2] == "x") ||
-          (chart[3] == "x" && chart[4] == "x" && chart[5] == "x") ||
-          (chart[6] == "x" && chart[7] == "x" && chart[8] == "x") ||
-          (chart[0] == "x" && chart[3] == "x" && chart[6] == "x") ||
-          (chart[1] == "x" && chart[4] == "x" && chart[7] == "x") ||
-          (chart[2] == "x" && chart[5] == "x" && chart[8] == "x") ||
-          (chart[0] == "x" && chart[4] == "x" && chart[8] == "x") ||
-          (chart[2] == "x" && chart[4] == "x" && chart[6] == "x")
-        ) {
-          alert("x won");
-        } else if (
-          (chart[0] == "o" && chart[1] == "o" && chart[2] == "o") ||
-          (chart[3] == "o" && chart[4] == "o" && chart[5] == "o") ||
-          (chart[6] == "o" && chart[7] == "o" && chart[8] == "o") ||
-          (chart[0] == "o" && chart[3] == "o" && chart[6] == "o") ||
-          (chart[1] == "o" && chart[4] == "o" && chart[7] == "o") ||
-          (chart[2] == "o" && chart[5] == "o" && chart[8] == "o") ||
-          (chart[0] == "o" && chart[4] == "o" && chart[8] == "o") ||
-          (chart[2] == "o" && chart[4] == "o" && chart[6] == "o")
-        ) {
-          alert("o won");
-        } else if (!chart.includes("")) {
-          alert("tie");
-        }
-      });
+      cell.textContent = "";
     });
-  }
+    player1Play.style.cssText = "";
+    player2Play.style.cssText = "";
+    chartDiv.style.cssText = "display: grid";
+    gameOverMessage.style.cssText = "display: none";
+  });
 
-  //return values
-  return {
-    chart,
-    oxPlayer1Choice,
-    oxPlayer2Choice,
-  };
+  let chartCellList = document.querySelectorAll(".chartCell");
+  chartCellList.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      gamePlay(e);
+    });
+  });
+
+  function gamePlay(e) {
+    if (markCounter.length == 0) {
+      markCounter.push(oxPlayer1Choice);
+      e.target.textContent = oxPlayer1Choice;
+      player2Play.style.cssText = "color: red";
+      chart[parseInt(e.target.id)] = oxPlayer1Choice;
+    } else if (markCounter[markCounter.length - 1] === "x") {
+      markCounter.push("o");
+      e.target.textContent = oxPlayer2Choice;
+      player2Play.style.cssText = "";
+      player1Play.style.cssText = "color: red";
+      chart[parseInt(e.target.id)] = "o";
+    } else if (markCounter[markCounter.length - 1] === "o") {
+      markCounter.push("x");
+      e.target.textContent = oxPlayer1Choice;
+      player1Play.style.cssText = "";
+      player2Play.style.cssText = "color: red";
+      chart[parseInt(e.target.id)] = "x";
+    }
+    if (
+      (chart[0] == "x" && chart[1] == "x" && chart[2] == "x") ||
+      (chart[3] == "x" && chart[4] == "x" && chart[5] == "x") ||
+      (chart[6] == "x" && chart[7] == "x" && chart[8] == "x") ||
+      (chart[0] == "x" && chart[3] == "x" && chart[6] == "x") ||
+      (chart[1] == "x" && chart[4] == "x" && chart[7] == "x") ||
+      (chart[2] == "x" && chart[5] == "x" && chart[8] == "x") ||
+      (chart[0] == "x" && chart[4] == "x" && chart[8] == "x") ||
+      (chart[2] == "x" && chart[4] == "x" && chart[6] == "x")
+    ) {
+      chartDiv.style.cssText = "display: none";
+      gameOverMessage.style.cssText = "display: flex";
+      gameOverMessageContent.textContent = "X won";
+    } else if (
+      (chart[0] == "o" && chart[1] == "o" && chart[2] == "o") ||
+      (chart[3] == "o" && chart[4] == "o" && chart[5] == "o") ||
+      (chart[6] == "o" && chart[7] == "o" && chart[8] == "o") ||
+      (chart[0] == "o" && chart[3] == "o" && chart[6] == "o") ||
+      (chart[1] == "o" && chart[4] == "o" && chart[7] == "o") ||
+      (chart[2] == "o" && chart[5] == "o" && chart[8] == "o") ||
+      (chart[0] == "o" && chart[4] == "o" && chart[8] == "o") ||
+      (chart[2] == "o" && chart[4] == "o" && chart[6] == "o")
+    ) {
+      chartDiv.style.cssText = "display: none";
+      gameOverMessage.style.cssText = "display: flex";
+      gameOverMessageContent.textContent = "O won";
+    } else if (!chart.includes("")) {
+      chartDiv.style.cssText = "display: none";
+      gameOverMessage.style.cssText = "display: flex";
+      gameOverMessageContent.textContent = "Tie";
+    }
+  }
 })();
 
 /*let player = (oOrX) => {
